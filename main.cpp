994fe21c6f75e7dd5dbe146d7c8b380e8dc12d13
PerfektKey <58395648+PerfektKey@users.noreply.github.com>
2024-10-05 13:19:10 +0200

    Added sprite with tiling

--- main.cpp
+++ main.cpp
@@ -1,11 +1,19 @@
 #include <iostream>
 #include <SFML/Graphics.hpp>
+#include <cmath>
 
 int main () {
 	
 	// creating window
 	sf::RenderWindow window(sf::VideoMode(1000,800), "Bakteria Game");
 	
+	//standart tile
+	sf::RectangleShape tile;
+	tile.setSize(sf::Vector2f(50,50));
+	tile.setOutlineColor(sf::Color::Black);
+	tile.setOutlineThickness(5);
+
+	sf::Vector2i mouseP = sf::Mouse::getPosition();
 
 	// Game loop
 	while (window.isOpen()) {
@@ -17,9 +25,14 @@ int main () {
 				window.close();
 		}
 
+		mouseP = sf::Mouse::getPosition(window);
+		sf::Vector2f tileP = sf::Vector2f( std::floor(mouseP.x/50)*50 , std::floor(mouseP.y/50)*50 ); 
+		tile.setPosition(tileP);
+
 		
 		// updating the frame
 		window.clear(sf::Color(180,180,180));
+		window.draw(tile);
 		window.display();
 	}
 	
